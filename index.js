13a01e852665c8c1247cc68f049065a20d525f4a
Muhammadsharifbek <sharifahmadov690@gmail.com>
2023-06-28 14:06:56 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// let answer = [0];
+// let question = ["what is your name", " what is your phone", " how old are you", "how are you"];
+// let i = 0;
+// while (i < question.length) {
+//   answer[i] = prompt(question[i]);
+//   i++;
+// }
+// console.log(answer);
+
+// let a = 9;
+// let sum = 0;
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// let a = 9;
+// let sum = 0;
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// let a = 18;
+// let sum = 0;
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 5 == 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
